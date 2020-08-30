649a4086a86d27eb22b9ca6af6e965315296c4fc
Mohamed Atef <eng.mohamedatef98@gmail.com>
2020-08-30 11:25:26 +0200

    Shuffle Questions in Poll Components

--- components/Poll.tsx
+++ components/Poll.tsx
@@ -1,14 +1,23 @@
 import * as React from 'react';
+import { shuffle } from 'lodash';
 import styled from 'styled-components';
-import { QandAsDocument } from '../types';
+import { QandAsDocument, QandA } from '../types';
 
 type Props = {
   qandas: QandAsDocument /* q and a's -- questions and answers document */;
 };
 
+const shuffleQandAs = (qandas: QandA[]) => shuffle(qandas);
+
 const PollWrapper = styled.div``;
 
 export default function Poll({ qandas }: Props) {
-  console.log('questions and answers: ', qandas);
+  const [shuffledQandAs, setSuffledQandAs] = React.useState(shuffleQandAs(qandas.questions));
+
+  React.useEffect(
+    () => setSuffledQandAs(shuffleQandAs(qandas.questions)),
+    [qandas.questions]
+  );
+
   return <PollWrapper>The Poll implementation goes here</PollWrapper>;
 }
