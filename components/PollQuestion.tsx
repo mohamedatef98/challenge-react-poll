c0f4411da4ad930ba7b671682791c06ac8dca82c
Mohamed Atef <eng.mohamedatef98@gmail.com>
2020-08-30 11:41:28 +0200

    Display Question Header and Container

--- components/PollQuestion.tsx
+++ components/PollQuestion.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import styled from 'styled-components';
 import { shuffle } from 'lodash';
 import { QandA, Answer } from '../types';
 
@@ -8,13 +9,22 @@ type Props = {
 
 const shuffleAnswers = (answers: Answer[]) => shuffle(answers);
 
+const PollQuestionContainer = styled.div`
+    margin: 2rem auto;
+    max-width: 400px;
+    padding: 1rem;
+    border-radius: 5px;
+    box-shadow: 0 0 10px #ccc;
+
+`
+const PollQuestionHeader = styled.h2``
+
 export default function PollQuestion ({ qanda }: Props) {
-    const [, setSuffledAnswers] = React.useState(shuffleAnswers(qanda.answers));
+    const [,] = React.useState(shuffleAnswers(qanda.answers));
 
-    React.useEffect(
-        () => setSuffledAnswers(shuffleAnswers(qanda.answers)),
-        [qanda.answers]
-    )
-
-    return <div>PollQuestion</div>
+    return <PollQuestionContainer>
+        <PollQuestionHeader>
+            {qanda.question.text}
+        </PollQuestionHeader>
+    </PollQuestionContainer>
 }
